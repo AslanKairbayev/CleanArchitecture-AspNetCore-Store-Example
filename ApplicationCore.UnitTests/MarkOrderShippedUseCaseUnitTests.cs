2519e9308370df4d7ce7021d371cea16719322d4
aslan_kairbayev <aslan_kairbayev@mail.ru>
2020-04-06 02:55:59 +0500

    MarkOrderShippedUseCase

---
+++ ApplicationCore.UnitTests/MarkOrderShippedUseCaseUnitTests.cs
@@ -0,0 +1,66 @@
+﻿using ApplicationCore.Dto.RepositoryResponses.OrderRepository;
+using ApplicationCore.Dto.UseCaseRequests;
+using ApplicationCore.Dto.UseCaseResponses;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Interfaces.Repositories;
+using ApplicationCore.UseCases;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApplicationCore.UnitTests
+{
+    public class MarkOrderShippedUseCaseUnitTests
+    {
+        [Fact]
+        public void Can_Mark_Order_Shipped()
+        {
+            var mockOrderRepository = new Mock<IOrderRepository>();
+
+            mockOrderRepository
+                .Setup(repo => repo.GetOrderById(It.IsAny<int>()))
+                .Returns(new Order());
+
+            mockOrderRepository
+                .Setup(repo => repo.Update(It.IsAny<Order>()))
+                .Returns(new UpdateOrderResponse(true));
+
+            var useCase = new MarkOrderShippedUseCase(mockOrderRepository.Object);
+
+            var mockOutputPort = new Mock<IOutputPort<MarkOrderShippedResponse>>();
+
+            mockOutputPort.Setup(outputPort => outputPort.Handle(It.IsAny<MarkOrderShippedResponse>()));
+
+            var response = useCase.Handle(new MarkOrderShippedRequest(), mockOutputPort.Object);
+
+            Assert.True(response);
+        }
+
+        [Fact]
+        public void Cant_Mark_Order_Shipped_When_Order_Not_Found()
+        {
+            var mockOrderRepository = new Mock<IOrderRepository>();
+
+            mockOrderRepository
+                .Setup(repo => repo.GetOrderById(It.IsAny<int>()))
+                .Returns(It.IsAny<Order>());
+
+            mockOrderRepository
+                .Setup(repo => repo.Update(It.IsAny<Order>()))
+                .Returns(new UpdateOrderResponse(true));
+
+            var useCase = new MarkOrderShippedUseCase(mockOrderRepository.Object);
+
+            var mockOutputPort = new Mock<IOutputPort<MarkOrderShippedResponse>>();
+
+            mockOutputPort.Setup(outputPort => outputPort.Handle(It.IsAny<MarkOrderShippedResponse>()));
+
+            var response = useCase.Handle(new MarkOrderShippedRequest(), mockOutputPort.Object);
+
+            Assert.False(response);
+        }
+    }
+}
--- ApplicationCore/Dto/RepositoryResponses/OrderRepository/UpdateOrderResponse.cs
+++ ApplicationCore/Dto/RepositoryResponses/OrderRepository/UpdateOrderResponse.cs
@@ -7,7 +7,7 @@ namespace ApplicationCore.Dto.RepositoryResponses.OrderRepository
 {
     public class UpdateOrderResponse : BaseRepositoryResponse
     {
-        public UpdateOrderResponse(int id, bool success = false) : base(success)
+        public UpdateOrderResponse(bool success = false) : base(success)
         {
             
         }
